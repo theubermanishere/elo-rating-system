9bc1d33d323abd510e3297bf98d9e3b9a18c7b8a
Vinayak <vinayakkaushikk@gmail.com>
2017-11-05 22:34:34 +0530

    Basic system implemented.

---
+++ elo.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+""" An implementation of the Elo rating system to be used for ranking during 
+competitions
+"""
+
+from math import pow
+
+WIN = 1
+LOSS = 0
+DRAW = 0.5
+
+class Elo:
+    """ Creates a object that contains a particular participants information
+    and other related methods """
+
+    games = 0
+
+    def __init__(self, rank=1400, k=32):
+        self.rank = rank
+        self.k = k
+
+    def expectancy(self, b):
+        exp = 1 / (1 + pow(10, (b - self.rank) / 400))
+        return exp
+
+    def winsAgainst(self, b):
+        self.rank = self.rank + self.k * (WIN - self.expectancy(b.rank))
+        b.rank = b.rank + self.k * (LOSS - b.expectancy(self.rank))
+
+    def loseAgainst(self, b):
+        self.rank = self.rank + self.k * (LOSS - self.expectancy(b.rank))
+        b.rank = b.rank + self.k * (WIN - b.expectancy(self.rank))
+
+    def drawAgainst(self, b):
+        self.rank = self.rank + self.k * (DRAW - self.expectancy(b.rank))
+        b.rank = b.rank + self.k * (DRAW - b.expectancy(self.rank))
+
+if __name__ == "__main__":
+    a = Elo(1300)
+    b = Elo(1500)
+    print(a.rank)
+    a.winsAgainst(b)
+    print(a.rank)
+    print(b.rank)
+    a.loseAgainst(b)
+    print(a.rank)
+    print(b.rank)
+    a.drawAgainst(b)
+    print(a.rank)
+    print(b.rank)
+
