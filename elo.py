4b1ea87c5713473925e2ccdd78a49479c3401881
Vinayak <vinayakkaushikk@gmail.com>
2017-11-06 10:17:47 +0530

    Rounding error still present

--- elo.py
+++ elo.py
@@ -12,9 +12,12 @@ DRAW = 0.5
 
 class Elo:
     """ Creates a object that contains a particular participants information
-    and other related methods """
+    and other related methods.
 
-    games = 0
+    usage: obj = Elo(rank=1400,k=32)
+    """
+
+    game = 0
 
     def __init__(self, rank=1400, k=32):
         self.rank = rank
@@ -26,18 +29,36 @@ class Elo:
         return exp
 
     def winsAgainst(self, b):
-        """ Calculates and changes rank of both self and opponent. """
-        self.rank = self.rank + floor(self.k * (WIN - self.expectancy(b.rank)))
-        b.rank = b.rank + floor(self.k * (LOSS - b.expectancy(self.rank)))
+        """ Calculates and changes rank of both self and opponent 
+        if self wins. """
+        self.game = self.game + 1
+        b.game = b.game + 1
+        self.rank = self.rank + int(round(self.k * (WIN 
+            - self.expectancy(b.rank))))
+        b.rank = b.rank + int(round(self.k * (LOSS - b.expectancy(self.rank))))
 
     def loseAgainst(self, b):
-        self.rank = self.rank + floor(self.k * (LOSS - self.expectancy(b.rank)))
-        b.rank = b.rank + floor(self.k * (WIN - b.expectancy(self.rank)))
+        """ Calculates and changes rank of both self and opponent
+        if self loses. """
+        self.game = self.game + 1
+        b.game = b.game + 1
+        self.rank = self.rank + int(round(self.k * (LOSS 
+            - self.expectancy(b.rank))))
+        b.rank = b.rank + int(round(self.k * (WIN - b.expectancy(self.rank))))
 
     def drawAgainst(self, b):
-        self.rank = self.rank + floor(self.k * (DRAW - self.expectancy(b.rank)))
-        b.rank = b.rank + floor(self.k * (DRAW - b.expectancy(self.rank)))
+        """ Calculates and changes rank of both self and opponent
+        if the match is a draw. """
+        self.game = self.game + 1
+        b.game = b.game + 1
+        self.rank = self.rank + int(round(self.k * (DRAW 
+            - self.expectancy(b.rank))))
+        b.rank = b.rank + int(round(self.k * (DRAW - b.expectancy(self.rank))))
 
+    def games(self):
+        return (self.games)
+
+# Sample Implementation
 if __name__ == "__main__":
     a = Elo(1300)
     b = Elo(1500)
